da042e4a8d1e6ec1dc2c492840548f50a67ea241
spookypeanut <github@spookypeanut.co.uk>
2018-05-25 15:31:35 +0100

    Add destructor

--- python/itsatrap/pir.py
+++ python/itsatrap/pir.py
@@ -7,8 +7,12 @@ class Pir(object):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(PIR_PIN, GPIO.IN)
 
+    def __del__(self):
+        GPIO.cleanup()
+
     def start_watching(self, on_trap_call):
         GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=on_trap_call)
 
     def stop_watching(self):
         GPIO.remove_event_detect(PIR_PIN)
+
