1a5cc8cc07dfe1265c39e32e59bc722c586e31d5
spookypeanut <github@spookypeanut.co.uk>
2019-07-28 21:45:46 +0100

    Add some docs

--- python/itsatrap/pir.py
+++ python/itsatrap/pir.py
@@ -5,23 +5,34 @@ PIR_PIN = 7
 NUM_TIMES = 3
 NUM_SECONDS = 1
 
+
 class Pir(object):
     def __init__(self):
+        # The pin numbering system to use
         GPIO.setmode(GPIO.BCM)
+        # Tell it which input pin to use
         GPIO.setup(PIR_PIN, GPIO.IN)
+        # previous contains a list of the datetimes of all the triggers
+        # in the last NUM_SECONDS (when using buffered_call)
         self.previous = []
+        # The function to call when the IR detector is triggered
         self.on_trap_call = None
 
     def __del__(self):
         GPIO.cleanup()
 
     def _clear_old(self):
+        """ Delete any trigger events from self.previous that are too old """
+        # The oldest that events in the previous list can be
         oldest = datetime.now() - timedelta(seconds=NUM_SECONDS)
         for d in self.previous:
             if d < oldest:
                 self.previous.remove(d)
 
     def buffered_call(self, on_trap_call):
+        """ Only call the triggered function if the detector is triggered
+        NUM_TIMES times within NUM_SECONDS seconds.
+        """
         self.previous.append(datetime.now())
         self._clear_old()
         print(self.previous)
