89bf173b62a16683c080718f60b8d6758fa81da3
spookypeanut <github@spookypeanut.co.uk>
2019-07-28 20:54:28 +0100

    Dryrun, for testing

--- python/itsatrap/trap.py
+++ python/itsatrap/trap.py
@@ -8,16 +8,25 @@ from .camera import Camera
 
 DEFAULT_PHOTO_PATH = "/home/hbush/trapphotos"
 
+
 class Trap(object):
-    def __init__(self):
+    def __init__(self, photos=True, verbose=False):
         self.pir = Pir()
-        self.camera = Camera()
+        self.take_photos = photos
+        self.verbose = verbose
+        if self.take_photos is True:
+            self.camera = Camera()
+        else:
+            self.camera = None
 
     def start_trap(self):
         def trap_function(pir_pin):
             base_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S.jpg")
             image_path = os.path.join(DEFAULT_PHOTO_PATH, base_name)
-            self.camera.take_photo(image_path)
+            if self.take_photos is True:
+                self.camera.take_photo(image_path)
+            if self.verbose is True:
+                print("Triggered: %s" % image_path)
 
         self.pir.start_watching(trap_function)
 
